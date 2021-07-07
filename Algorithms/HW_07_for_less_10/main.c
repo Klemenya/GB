2d84cc1d898db4bbd9473cba51daf8068709cbc2
Klemenya <Klemenya@gmail.com>
2021-07-07 07:35:30 +0300

    add task2 and task3 check logic in task1

--- Algorithms/HW_07_for_less_10/main.c
+++ Algorithms/HW_07_for_less_10/main.c
@@ -79,15 +79,146 @@ char checkRevP(char c) {
     case '}':
         return '{';
     }
+    return c;
 }
+
+
+
+/*--------------------- singly linked list -------------------*/
+typedef struct {
+    Node *head;
+    int size;
+} List;
+
+void ins(List *lst, T data) {
+    Node *new = (Node*) malloc(sizeof(Node*));
+    new->dat = data;
+    new->next = NULL;
+
+    Node *current = lst->head;
+    if (current == NULL) {
+        lst->head = new;
+        lst->size++;
+        return;
+    } else
+    {
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new;
+        lst->size++;
+    }
+}
+
+Node* rm(List *lst, T data) {
+    Node *current = lst->head;
+    Node *parrent = NULL;
+
+    if (current == NULL) {
+        return NULL;
+    }
+
+    while (current->next != NULL && current->dat != data) {
+        parrent = current;
+        current = current->next;
+    }
+
+    if (current->dat != data) {
+        return NULL;
+    }
+
+    if (current == lst->head) {
+        lst->head = current->next;
+        lst->size--;
+        return current;
+    }
+
+    parrent->next = current->next;
+    lst->size--;
+    return current;
+}
+
+void printNode(Node *n) {
+    if (n == NULL) {
+        printf("[]");
+        return;
+    }
+    printf("[%d] ", n->dat);
+}
+
+void printList(List *lst) {
+    Node *current = lst->head;
+    if (current == NULL) {
+        printNode(current);
+    } else
+    {
+        do {
+            printNode(current);
+            current = current->next;
+        } while (current != NULL);
+    }
+    printf("\n");
+}
+
+List* list_copy(List *lst) {
+    List *tmp = (List*) malloc(sizeof(List*));
+    Node *current_node = (Node*) malloc(sizeof(Node*));
+
+    if (lst->size == 0) {
+        init_list(tmp);
+        return tmp;
+    }
+
+    current_node = lst->head;
+
+    for (int i = 0; i < lst->size; ++i) {
+        ins(tmp, current_node->dat);
+        current_node = current_node->next;
+    }
+    free(current_node);
+
+    return tmp;
+}
+
+boolean isSorted(List *lst) {
+    if (lst->size == 0) {
+        return -1;
+    }
+
+    Node *current_node = (Node*) malloc(sizeof(Node*));
+    current_node = lst->head;
+
+    if (current_node->next == NULL) {
+        free(current_node);
+        return true;
+    }
+
+    for (int i = 0; i < lst->size - 1; ++i) {
+        if (current_node->dat > current_node->next->dat) {
+            free(current_node);
+            return false;
+        }
+    }
+    free(current_node);
+    return true;
+}
+
+void init_list(List* lst) {
+    lst->head = NULL;
+    lst->size =0;
+}
+
+
 
 int main() {
 
-    { /* ------------  task 10.1 -------------------*/
+     /* ------------  task 10.1 -------------------*/
     Stack *st = (Stack*) malloc(sizeof(Stack));
     init(st);
 
     char str[100];
+    int balance = 0;
+    printf("Enter sequence: ");
     fgets(str, 100, stdin);
     int n = strlen(str)-1;
 
@@ -95,24 +226,64 @@ int main() {
         if(checkSymbolOpen(str[i]))
         {
             push(st,str[i]);
+            balance++;
         }
 
         if(checkSymbolClose(str[i]))
         {
             if(st == NULL || read(st) != checkRevP(str[i]) )
             {
-                printf("Bracket sequence is NO CORRECT\n");
-                return 0;
+                balance = -1;
+                break;
             }
             else
             {
                 pop(st);
+                balance--;
             }
 
         }
     }
-    printf("Bracket sequence is CORRECT\n");
+    if(balance !=0){
+        printf("Bracket sequence is NO CORRECT\n\n");
+    } else
+    {
+    printf("Bracket sequence is CORRECT\n\n");
     }
+
+     /* ------------  task 10.2 -------------------*/
+    List *lst = (List*) malloc(sizeof(List*));
+    List *lst_copy;
+
+    init_list(lst);
+    //ins(lst, 1);
+    //ins(lst, 22);
+    //ins(lst, 10);
+    //ins(lst, 12);
+
+    ins(lst, 1);
+    ins(lst, 2);
+    ins(lst, 3);
+    ins(lst, 4);
+
+    printf("Original singly linked list:\n");
+    printList(lst);
+
+    lst_copy = list_copy(lst);
+    printf("Copied singly linked list:\n");
+    printList(lst_copy);
+
+    rm(lst_copy, 1);
+    printf("Modifying the copied singly linked list:\n");
+    printf("Original:\n");
+    printList(lst);
+    printf("Copied:\n");
+    printList(lst_copy);
+
+    /* ------------  task 10.3 -------------------*/
+    printf("This list is%ssorted\n", isSorted(lst) == 1 ? " " : " NOT ");
+    free(lst);
+    free(lst_copy);
 
     return 0;
 }
