df851d696edd6b0ebec4f3048bf14d4bf7518e22
unknown <Klemenya@gmail.com>
2021-11-25 22:32:39 +0300

    add task 2

--- DifficultMoments/HW02/main.cpp
+++ DifficultMoments/HW02/main.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
+#include <vector>
+
+#include <iostream>
+#include <any>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
 
 using namespace std;
 
 template <typename T>
-void Swap (T &a, T &b)
-{
+void Swap (T &a, T &b){
     T temp = a;
     a = b;
     b = temp;
 }
-
 template <typename T1>
 void Swap2(T1** a, T1** b) {
 		T1* temp = *a;
 		*a = *b;
 		*b = temp;;
 }
-void printVariable (int a, int b, int* pA, int* pB, int** ppA, int** ppB)
-{
+void printVariable (int a, int b, int* pA, int* pB, int** ppA, int** ppB){
     cout << "A = " << a << " ["<<&a<<"] " <<"  " << "B = " << b << " ["<<&b<<"] " << '\t';
     cout << "*pA = " << " [" << *pA << "] " << "*pB = " << " [" << *pB << "] " <<'\t' ;
     cout << "**ppA = " << " [" << **ppA << "] " << "**ppB = " << " [" << **ppB << "] " <<'\n';
 }
 
-int main(){
+template <typename T>
+void vectorSort(vector <T>& v){
+	sort(v.begin(), v.end(), [](T left, T right)
+                                {return *left < *right;});
+}
 
+
+
+
+
+int main()
+{
+/*---------------------------------------------------------*/
     int a = 1;
     int b = 2;
     int* pA = &a;
@@ -37,7 +54,6 @@ int main(){
     cout << "--------Swap (ppA, ppB)----------------------\n";
     printVariable (a, b, pA, pB, ppA, ppB);
 
-
     pA = &a;
     pB = &b;
     ppA = &pA;
@@ -47,7 +63,17 @@ int main(){
     printVariable (a, b, pA, pB, ppA, ppB);
 
 
+    /*---------------------------------------------------------*/
+    vector<int*> myVector;
+    for(int i=0; i<10;++i)
+        myVector.push_back(new int(rand()%10));
 
+    for(auto v : myVector)
+        cout << v << ' ' << *v <<'\n';
 
-   cin.get();
+    vectorSort(myVector);
+    cout << "-------- After sort --------------\n";
+
+    for(auto v : myVector)
+        cout << v << ' ' << *v <<'\n';
 }
